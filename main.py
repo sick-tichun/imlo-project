8ca22b325257356fe5860592c45110571a98744a
sick-tichun <62257672+sick-tichun@users.noreply.github.com>
2024-05-03 20:20:22 +0100

    working nn now

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ torch.backends.cudnn.benchmark = True
 
 #defining a transformer for our training dataset to randomise
 #rotations, aspect ratios etc of the image, so the model isnt thrown off by these aspects
+size = 224
 t_transform = transforms.Compose([
     transforms.RandomRotation(30), #rotates the images randomly
-    transforms.RandomResizedCrop(size=224), #resizes/crops image
+    transforms.RandomResizedCrop(size=size), #resizes/crops image
     transforms.RandomHorizontalFlip(), #flips
     transforms.RandomVerticalFlip(),
     transforms.ToTensor(), 
@@ -24,20 +25,25 @@ t_transform = transforms.Compose([
 ])
 v_transfrom = transforms.Compose([
     transforms.Resize(256),
-    transforms.CenterCrop(224),
+    transforms.CenterCrop(size),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #squishes data into a range wich is vital to a CNN network to performing
-    ])
-
+])
+test_transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(size),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #squishes data into a range wich is vital to a CNN network to performing
+])
 #normalize to be possibly adjusted later, these are placehld values
 
 
 #loading our datast (with transformation applied)
-train_data = datasets.Flowers102(root='data/train',download=True, split='train', transform=t_transform, target_transform = Lambda(lambda label: torch.zeros(1020, dtype=torch.float).scatter_(dim=0, index=torch.tensor(label), value=1)))
+train_data = datasets.Flowers102(root='data/train',download=True, split='train', transform=t_transform)
 valid_data = datasets.Flowers102(root='data/valid',download=True, split='val', transform=v_transfrom)
-test_data = datasets.Flowers102(root='data/test',download=True, split='test')
+test_data = datasets.Flowers102(root='data/test',download=True, split='test', transform=test_transform)
 
-batch_size = 200
+batch_size = 16
 
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=0)
 valid_loader = torch.utils.data.DataLoader(valid_data, batch_size=batch_size, shuffle=False, num_workers=0)
@@ -45,11 +51,12 @@ test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuf
 
 #implementing the NN given in the lecture (this will be changed to a convelutional neural network later)
 
-class My_NN(nn.Module):
-    def __init__(self, in_feature, hidden_layers, out_features, activation_function = F.relu):
+class network(nn.Module):
+    def __init__(self, in_feature, hidden_layers, out_features):
         super().__init__()
         if len(hidden_layers) < 1:
             raise Exception("gotta have more than 1 hidden layer")
+        self.flatten = nn.Flatten()
         
         self.layers = []
         self.layers.append(nn.Linear(in_feature, hidden_layers[0]))
@@ -60,35 +67,64 @@ class My_NN(nn.Module):
             self.add_module('hidden_layer_{i}', self.layers[i])
 
         self.out = nn.Linear(hidden_layers[-1], out_features)
+        self.add_module('output layer', self.out)
+        self.activation_function = nn.ReLU()
+        print(self.layers, self.out)
 
-        self.activation_function = activation_function
-    
     def forward(self, x):
+        x = self.flatten(x)
         for i in range(len(self.layers)):
             x = self.activation_function(self.layers[i](x))
-        x = self.out(x)
+        x = self.out(x).to(device)
         return x
 
-print(len(train_data._labels))
-classifier = My_NN(in_feature=224, hidden_layers=[32, 16], out_features=len(train_data._labels)).to(device)
+
+classifier = network(in_feature=size*size*3, hidden_layers=[2048, 1024, 1024, 512], out_features=102).to(device)
 loss_func = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(classifier.parameters(), lr=0.001)
-epochs = 20
+
+epochs = 10
+
+def train(loader, model=classifier, loss_func = loss_func, optimizer = optimizer):
+    model.train()
+    print('asdasdaasdd asasd'+str(next(model.parameters()).is_cuda))
+    size = len(loader.dataset)
+    for batch, (X, y) in enumerate(loader):
+        X, y = X.to(device), y.to(device)
+        print(X.is_cuda)
+        pred = model(X)
+        loss = loss_func(pred, y)
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        
+        if batch % 80:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
-losses = []
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
 for i in range(epochs):
-    for batch, label in train_loader:
-        batch, label = batch.to(device), label.to(device)
-        batch = torch.argmax(batch, dim=1)
-        outputs = classifier.forward(batch)
-        print(label)
-        loss = loss_func(outputs, label)
-    losses.append(loss)
-    if i % 10 == 0:
-        print('epoch' + str(i) + str(loss))
-
-    optimizer.zero_grad()
-    loss.backwards()
-    optimizer.step()
-
+    print(f"Epoch {i+1}\n-------------------------------")
+    train(train_loader, classifier)
+    if i % 5 == 0 and i != 0:
+        test(valid_loader, classifier, loss_func)
+print("Done!")
+test(test_loader, classifier, loss_func)
+torch.save(classifier.state_dict(), 'model.pth')
