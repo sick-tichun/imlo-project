56e0efccf0cd10a3143aaf1a2cb9f3d230fe28cd
sick-tichun <62257672+sick-tichun@users.noreply.github.com>
2024-05-27 07:38:42 +0100

    load update

--- load.py
+++ load.py
@@ -1,4 +1,4 @@
-#script to load and test the model against teh 
+#importing hte neccecary libraries
 import torch
 import torchvision
 from torchvision import datasets, transforms
@@ -7,23 +7,97 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.transforms import ToTensor, Lambda
 
+
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if use_cuda else "cpu")
 print(device)
+torch.cuda.empty_cache()
 
+
+
+#defining a transformer for our training dataset to randomise
+#rotations, aspect ratios etc of the image, so the model isnt thrown off by these aspects
 size = 224
-batch_size = 16
+
 test_transform = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(size),
     transforms.ToTensor(),
     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
 ])
-test_data = datasets.Flowers102(root='data/test',download=True, split='test', transform=test_transform)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=0)
+#Normalise used precalculated values from https://howieko.com/post/classifying_flowers_pytorch/
 
-classifier = torch.load('model.pth')
+
+#loading our datast (with transformation applied)
+test_data = datasets.Flowers102(root='data/test',download=True, split='test', transform=test_transform)
+
+batch_size = 16
+
+
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=8)
+
+#implementing a CNN
+class Cnetwork(nn.Module):
+    def __init__(self, in_channels = 3, out_features=102, size = 224):
+        super().__init__()
+        self.out_features = out_features
+        self.stack = nn.Sequential(
+            # "same" convolution layer (ie input feat = output feat)
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3), stride=(1,1), padding=(1,1)),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3), stride=(1,1), padding=(1,1)),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2,2), stride=(2,2)),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), stride=(1,1), padding=(1,1)),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3,3), stride=(1,1), padding=(1,1)),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            #reduces the size of the image by half
+            nn.MaxPool2d(kernel_size=(2,2), stride=(2,2)),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3,3), stride=(1,1), padding=(1,1)),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=(1,1), padding=(1,1)),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=(1,1), padding=(1,1)),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2,2), stride=(2,2)),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3,3), stride=(1,1), padding=(1,1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=(1,1), padding=(1,1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        )
+        self.fully_con = nn.Sequential(
+            nn.Linear(100352, 2048),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(2048, 2048),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(2048, self.out_features)
+        )
+
+    def forward(self, x):
+        x = self.stack(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fully_con(x)
+        return x
+
+
+#classifier = network_old(in_feature=size*size*3, hidden_layers=[2048, 512], out_features=102).to(device)
+classifier = Cnetwork(size=size).to(device)
 loss_func = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(classifier.parameters(), lr=0.001, weight_decay=0.005)
+
 
 def test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
@@ -39,5 +113,6 @@ def test(dataloader, model, loss_fn):
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
 
 test(test_loader, classifier, loss_func)
